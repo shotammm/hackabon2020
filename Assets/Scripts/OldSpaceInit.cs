174c39150fb3e59eee24f0343a79eb4af6676d6f
南 翔太 <shotaminami@shotaminami-no-MacBook-Pro.local>
2020-08-10 20:37:12 +0900

    Scriptsの編集など

--- Assets/Scripts/OldSpaceInit.cs
+++ Assets/Scripts/OldSpaceInit.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OldSpaceInit : MonoBehaviour {
 
     public GameObject boxObjPrefab;
     public GameObject boxesObj;
+    public static int oldSpaceNum = 10;
+    public float nowTime;
 
     void Awake() {
-        for (int x = 0; x < 10; x++) {
+        for (int x = 0; x < oldSpaceNum; x++) {
             float X = Random.Range (-2.5f, 2.5f);
             float Y = Random.Range (-1.0f, 1.0f);
             float Z = Random.Range (-2.5f, 2.5f);
@@ -19,11 +22,19 @@ public class OldSpaceInit : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-
+        nowTime = 0;
     }
 
     // Update is called once per frame
     void Update () {
+        nowTime += Time.deltaTime;
+        if (oldSpaceNum <= 0) {
+            GameOver("CONGRATULATIONS! \n YOU CLEARED IN " + nowTime.ToString("F0") + " SECONDS");  //<=====秒数をstring型にキャストして引数へ
+        }
+    }
 
+    public void GameOver(string resultMessage) {   //<=====引数をもたせた
+        DataSender.resultMessage = resultMessage;  //<=====受け取った引数をstatic変数へ格納
+        SceneManager.LoadScene("Result");
     }
 }
